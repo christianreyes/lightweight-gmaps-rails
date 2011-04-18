318c65813e3f205a5ef59c1e205c6d569635fe5c
Christian Reyes <cjreyes@andrew.cmu.edu>
2011-04-18 04:02:43 -0400

    installed jquery. remember to remove open ssh setting in environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,12 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+#for quickfix, add following lines to "environment.rb":
+
+require 'openssl'
+OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
+
+#make sure that you remove those lines after you install jquery-rails
+
 # Initialize the rails application
 GoogleMapsRails::Application.initialize!
