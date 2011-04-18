d61c819f0404eeb90674f938fa9c3955e0afe845
Christian Reyes <cjreyes@andrew.cmu.edu>
2011-04-18 04:56:15 -0400

    lightweight Google Maps Javascript API V3 with Ruby on Rails

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,20 @@
 class HomeController < ApplicationController
   def index
+	if params[:latitude]
+		@latitude = params[:latitude]
+	else
+		@latitude = 40.44260328858159
+	end
+	if params[:longitude]
+		@longitude = params[:longitude]
+	else
+		@longitude = -79.94316458702087
+	end
+	if params[:zoom]
+		@zoom = params[:zoom]
+	else
+		@zoom = 17
+	end
   end
 
 end
