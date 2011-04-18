257109def8e0ff3d37cb1e1f46ffb6f2a1d182e1
Christian Reyes <cjreyes@andrew.cmu.edu>
2011-04-18 03:55:15 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GoogleMapsRails::Application.config.secret_token = '006bb5f2cca5c2b645046244dc1180c88e86f1a8fc3992b1f01e49c4f26d17ccb58f769da53b76b2be0afbf9c04972903c5fa820f755470a35876291dec2cac5'
